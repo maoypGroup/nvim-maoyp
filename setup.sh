d237c9fadc1eaa53aae94ce4fff3f53a31021f6b
maoypGroup <maoyp@maoyp.onmicrosoft.com>
2022-02-17 02:07:25 +0800

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -43,11 +43,13 @@ then
     :
 else
 sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen -y
-git clone https://github.com/neovim/neovim ~
+git clone https://github.com/neovim/neovim 
 cd neovim && make
 sudo make install
 echo "alias vim='nvim'" >> ~/.bashrc
 source ~/.bashrc
+cd ../
+rm -rf neovim
 fi
 
 sudo apt-get install git -y
